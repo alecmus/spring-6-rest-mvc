521d332902083b2a87b422ebdc78900a67d21c69
Alec Musasa <alecmus@live.com>
2023-05-15 08:23:37 +0200

    Demonstrating Lombok @Data

---
+++ src/main/java/com/github/alecmus/spring6restmvc/model/Beer.java
@@ -0,0 +1,20 @@
+package com.github.alecmus.spring6restmvc.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
