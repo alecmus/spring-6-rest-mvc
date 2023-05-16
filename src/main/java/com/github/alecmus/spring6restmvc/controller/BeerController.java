2fe37bbac3c1eb2d67b8d16623116900ca1da516
Alec Musasa <alecmus@live.com>
2023-05-16 11:55:39 +0200

    Demonstrating setting HTTP header on response

--- src/main/java/com/github/alecmus/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/github/alecmus/spring6restmvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.github.alecmus.spring6restmvc.model.Beer;
 import com.github.alecmus.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +21,12 @@ public class BeerController {
 
     @PostMapping
     public ResponseEntity handlePost(@RequestBody Beer beer) {
+
         Beer savedBeer = beerService.saveNewBeer(beer);
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
