d5d1a14238f114fd6d2416c43a1f0ee4240f65a3
Alec Musasa <alecmus@live.com>
2023-05-17 14:24:47 +0200

    MockMVC Configuration

--- src/main/java/com/github/alecmus/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/github/alecmus/spring6restmvc/controller/BeerController.java
@@ -2,7 +2,7 @@ package com.github.alecmus.spring6restmvc.controller;
 
 import com.github.alecmus.spring6restmvc.model.Beer;
 import com.github.alecmus.spring6restmvc.services.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,10 +13,11 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
+
     private final BeerService beerService;
 
     @PatchMapping("{beerId}")
