610e618a3859150aede87754dc7ec8ecfe829fda
Alec Musasa <alecmus@live.com>
2023-05-19 17:48:29 +0200

    Global Exception handling using @ControllerAdvice

---
+++ src/main/java/com/github/alecmus/spring6restmvc/controller/ExceptionController.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.spring6restmvc.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+}
