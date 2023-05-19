e568d934a39e5303e58567fdd64bafd2577bff26
Alec Musasa <alecmus@live.com>
2023-05-19 17:54:11 +0200

    Global Exception handling using @ResponseStatus

--- src/main/java/com/github/alecmus/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/com/github/alecmus/spring6restmvc/controller/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.github.alecmus.spring6restmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
