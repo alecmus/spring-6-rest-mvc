0a546f366c60ccae69d2466a9ddb5331a4bc9c40
Alec Musasa <alecmus@live.com>
2023-05-19 17:24:32 +0200

    Throw custom Exception with Mockito

--- src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerTest.java
@@ -126,6 +126,15 @@ class BeerControllerTest {
     }
 
     @Test
+    void getBeerIdNotFound() throws Exception {
+
+        given(beerService.getBeerById(any(UUID.class))).willThrow(NotFoundException.class);
+
+        mockMvc.perform(get(BeerController.BEER_PATH_ID, UUID.randomUUID()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     void getBeerById() throws Exception {
         Beer testBeer = beerServiceImpl.listBeers().get(0);
 
