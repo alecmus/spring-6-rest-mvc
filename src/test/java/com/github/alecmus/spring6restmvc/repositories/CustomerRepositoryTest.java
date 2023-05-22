47314dd3413b504574b7914cba24cc7afa89bfd0
Alec Musasa <alecmus@live.com>
2023-05-22 17:37:50 +0200

    Added Spring Boot JPA Test Splice

---
+++ src/test/java/com/github/alecmus/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.github.alecmus.spring6restmvc.repositories;
+
+import com.github.alecmus.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer customer = customerRepository.save(Customer.builder()
+                .name("New Name")
+                .build());
+
+        assertThat(customer.getId()).isNotNull();
+    }
+}
