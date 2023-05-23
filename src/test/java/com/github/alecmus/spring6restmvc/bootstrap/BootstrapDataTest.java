76d5563694aa30c1aa1bcd3bc37adeff5e97b180
Alec Musasa <alecmus@live.com>
2023-05-23 16:07:26 +0200

    JPA bootstrap for populating data

---
+++ src/test/java/com/github/alecmus/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -0,0 +1,35 @@
+package com.github.alecmus.spring6restmvc.bootstrap;
+
+import com.github.alecmus.spring6restmvc.repositories.BeerRepository;
+import com.github.alecmus.spring6restmvc.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class BootstrapDataTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    BootstrapData bootstrapData;
+
+    @BeforeEach
+    void setUp() {
+        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+    }
+
+    @Test
+    void testRun() throws Exception {
+        bootstrapData.run(null);
+
+        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(customerRepository.count()).isEqualTo(3);
+    }
+}
