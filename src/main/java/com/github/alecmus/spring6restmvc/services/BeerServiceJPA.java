77d8539ff90c13f68a03d1c6423f61cfca9a2194
Alec Musasa <alecmus@live.com>
2023-05-23 16:40:24 +0200

    Added JPA Services

---
+++ src/main/java/com/github/alecmus/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,51 @@
+package com.github.alecmus.spring6restmvc.services;
+
+import com.github.alecmus.spring6restmvc.mappers.BeerMapper;
+import com.github.alecmus.spring6restmvc.model.BeerDTO;
+import com.github.alecmus.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return null;
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+}
---
+++ src/main/java/com/github/alecmus/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,51 @@
+package com.github.alecmus.spring6restmvc.services;
+
+import com.github.alecmus.spring6restmvc.mappers.CustomerMapper;
+import com.github.alecmus.spring6restmvc.model.CustomerDTO;
+import com.github.alecmus.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID uuid) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
