351096893db03f95ea00b50d8bba0dd90fe2eced
Alec Musasa <alecmus@live.com>
2023-05-23 19:29:45 +0200

    Testing for expected Exceptions

--- src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package com.github.alecmus.spring6restmvc.controller;
 
+import com.github.alecmus.spring6restmvc.entities.Beer;
 import com.github.alecmus.spring6restmvc.model.BeerDTO;
 import com.github.alecmus.spring6restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
 class BeerControllerIT {
@@ -20,6 +23,22 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerIdNotFound() {
+        assertThrows(NotFoundException.class, ()-> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+    }
 
     @Test
     void testListBeers() {
