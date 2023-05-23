7c241573f6750b3c48001a8fd00ab127c0247a7c
Alec Musasa <alecmus@live.com>
2023-05-23 18:05:30 +0200

    Controller integration test

---
+++ src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,40 @@
+package com.github.alecmus.spring6restmvc.controller;
+
+import com.github.alecmus.spring6restmvc.model.BeerDTO;
+import com.github.alecmus.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
