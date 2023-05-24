84cd478666f403bfa44164965e43143d770e741f
Alec Musasa <alecmus@live.com>
2023-05-24 12:57:36 +0200

    JPA Save new Beer operation

--- src/main/java/com/github/alecmus/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/github/alecmus/spring6restmvc/services/BeerServiceJPA.java
@@ -36,7 +36,7 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public BeerDTO saveNewBeer(BeerDTO beer) {
-        return null;
+        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beer)));
     }
 
     @Override
