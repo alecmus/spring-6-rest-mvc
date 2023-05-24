cc70f56df3b5e01392d063a8a295fa0901be13f6
Alec Musasa <alecmus@live.com>
2023-05-24 15:27:34 +0200

    JPA delete Beer by Id

--- src/main/java/com/github/alecmus/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/github/alecmus/spring6restmvc/services/BeerServiceJPA.java
@@ -61,7 +61,7 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public void deleteById(UUID beerId) {
-
+        beerRepository.deleteById(beerId);
     }
 
     @Override
