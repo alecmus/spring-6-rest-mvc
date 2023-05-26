24020c479cf9f9fa5fd0d5cc5e94600def2339e1
Alec Musasa <alecmus@live.com>
2023-05-26 05:28:43 +0200

    JPA validation

--- src/test/java/com/github/alecmus/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/github/alecmus/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.github.alecmus.spring6restmvc.repositories;
 
 import com.github.alecmus.spring6restmvc.entities.Beer;
+import com.github.alecmus.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,12 @@ class BeerRepositoryTest {
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                 .beerName("My Beer")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("6336739")
+                .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
