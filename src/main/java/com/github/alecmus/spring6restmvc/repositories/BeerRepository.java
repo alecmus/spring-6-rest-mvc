0a9b6c1b0a8b75df083412488b55372e61f5e0bb
Alec Musasa <alecmus@live.com>
2023-06-01 13:12:26 +0200

    Find by name with Spring Data JPA

--- src/main/java/com/github/alecmus/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/com/github/alecmus/spring6restmvc/repositories/BeerRepository.java
@@ -3,7 +3,10 @@ package com.github.alecmus.spring6restmvc.repositories;
 import com.github.alecmus.spring6restmvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
--- src/test/java/com/github/alecmus/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/github/alecmus/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,22 +1,34 @@
 package com.github.alecmus.spring6restmvc.repositories;
 
+import com.github.alecmus.spring6restmvc.bootstrap.BootstrapData;
 import com.github.alecmus.spring6restmvc.entities.Beer;
 import com.github.alecmus.spring6restmvc.model.BeerStyle;
+import com.github.alecmus.spring6restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@Import({BootstrapData.class, BeerCsvServiceImpl.class })
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(list.size()).isEqualTo(336);
+    }
 
     @Test
     void testSaveBeerNameTooLong() {
