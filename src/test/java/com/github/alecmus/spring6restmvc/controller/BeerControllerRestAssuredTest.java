ae4add8ad95f094605a3ca5858fb7abb369f0a9c
Alec Musasa <alecmus@live.com>
2023-07-13 03:32:12 +0200

    Added base code for RestAssured test
    Not working yet because the endpoint is secured, so we're getting a 401.

---
+++ src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -0,0 +1,32 @@
+package com.github.alecmus.spring6restmvc.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BeerControllerRestAssuredTest {
+
+    @LocalServerPort
+    Integer localPort;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = localPort;
+    }
+
+    @Test
+    void testListBeers() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/v1/beer")
+                .then()
+                .assertThat().statusCode(200);
+    }
+}
