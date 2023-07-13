3cf7d1600476fcb6f913f078a0a7a4e5ff8267b6
Alec Musasa <alecmus@live.com>
2023-07-13 03:40:26 +0200

    Spring security configuration for test
    Some advanced Spring configuration as follows:
    Setting the profile to "test" then disabling the Spring security configuration for the "test" profile (SecurityFilterChain).

--- src/main/java/com/github/alecmus/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/github/alecmus/spring6restmvc/config/SpringSecConfig.java
@@ -2,10 +2,12 @@ package com.github.alecmus.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecConfig {
 
--- src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/com/github/alecmus/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -6,11 +6,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages = "com.github.alecmus.spring6restmvc")
 public class BeerControllerRestAssuredTest {
+
+    public static class TestConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.authorizeHttpRequests().anyRequest().permitAll();
+
+            return http.build();
+        }
+    }
 
     @LocalServerPort
     Integer localPort;
